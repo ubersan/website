947052d6361b4ad27ddc81f47d507b604bae727d
Sandro Huber <sandrohuber89@hotmail.com>
2018-10-02 11:22:56 +0200

    Moved js into separate file

---
+++ public/externals.js
@@ -0,0 +1,9 @@
+var app = Elm.Main.init({
+    node: document.getElementById('elm-node')
+});
+
+app.ports.loadedMesh.send(read_obj_file());
+
+function read_obj_file() {
+    return 'from obj function';
+}
